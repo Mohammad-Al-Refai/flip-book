9d56742b40ef360b6cb3cc8604559c2b0e888a17
Mohammad-Al-Refai <m.shebli.refaai@gmail.com>
2024-03-24 21:06:42 +0300

    fix build

--- frontend/src/ui/components/If.ts
+++ frontend/src/ui/components/If.ts
@@ -1,10 +1,8 @@
-import { ReactNode } from "react";
-
 export function If({ condition, children }: IfProps) {
   return condition ? children : null;
 }
 
 interface IfProps {
   condition: boolean;
-  children: ReactNode;
+  children: JSX.Element;
 }
